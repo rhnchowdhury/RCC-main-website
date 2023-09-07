b5578cc99c8faab94e4c02cade4a19b51d66f00b
Raincloud <rhn.raincloud@gmail.com>
2023-09-07 19:27:08 +0600

    header edit done

--- src/Components/Shared/NavBar/Header/Header.js
+++ src/Components/Shared/NavBar/Header/Header.js
@@ -3,17 +3,20 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <div className='head-part'>
-            <div className='head-part-bg'>
-            </div>
-            <div >
-                <div>
-                    <h6 className='head-text-six'>ADVERTISING & MARKETING AGENCY</h6>
-                    <h1 className='head-text-one'>We help <br /> brands Grow <br /> Beautifully</h1>
+        <section className='head-section'>
+            <div className='head-part '>
+                <div className='head-part-bg'>
                 </div>
-                <button className="head-btn mt-2">GET STARTED NOW</button>
+                <div className='ml-32 pt-56'>
+                    <div >
+                        <h6 className='head-text-six'>ADVERTISING & MARKETING AGENCY</h6>
+                        <h1 className='head-text-one'>We help <br /> brands Grow <br /> Beautifully</h1>
+                    </div>
+                    <button className="head-btn mt-5 head-text">GET STARTED NOW</button>
+                </div>
+
             </div>
-        </div>
+        </section>
     );
 };
 export default Header;
