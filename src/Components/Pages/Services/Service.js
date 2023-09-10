8b28108da6d83625ab91f91d677832c82c675c9f
Raincloud <rhn.raincloud@gmail.com>
2023-09-10 18:24:53 +0600

    edit on service page

--- src/Components/Pages/Services/Service.js
+++ src/Components/Pages/Services/Service.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './service.css';
 import img1 from '../../../logo/add-operation.png';
-import img2 from '../../../logo/graphics.jpeg';
-import img3 from '../../../logo/eco.jpeg';
+import img2 from '../../../logo/graphics.png';
+import img3 from '../../../logo/eco.png';
+import img4 from '../../../logo/test.jpg';
 import { Link } from 'react-router-dom';
+import { BsFacebook, BsInstagram, BsLinkedin } from 'react-icons/bs';
 
 const Service = () => {
     return (
@@ -26,13 +28,34 @@ const Service = () => {
                 <div className='service-div'>
                     <div className='service-element'>
                         <img src={img3} alt="" className='service-icon text-white' />
-                        <h1 className='service-text'>Digital Eco</h1>
+                        <h1 className='service-text'>Digital Eco System</h1>
                         <p className='service-paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus.</p>
                     </div>
                 </div>
             </div>
             <div className='flex justify-center mt-5' >
                 <Link to='/service'><button className="service-btn">SEE ALL</button></Link>
+            </div>
+            <div>
+                <div className="hero mt-32">
+                    <div className="hero-content flex-col lg:flex-row-reverse">
+                        <div>
+                            <img src={img4} alt='' className="max-w-sm shadow-2xl" />
+                            <div className='mt-5'>
+                                <div className="space-x-3">
+                                    <button className=' service-social-btn '><Link to='https://www.facebook.com/Raincloudcommunications' target="_blank" rel="noopener noreferrer"><BsFacebook className='w-5 h-5 mx-auto'></BsFacebook></Link> </button>
+                                    <button className='service-social-btn'><Link to='https://www.instagram.com/raincloud.communications/' target="_blank" rel="noopener noreferrer"><BsInstagram className='w-5 h-5 mx-auto'></BsInstagram></Link></button>
+                                    <button className='service-social-btn'><Link to='https://www.linkedin.com/company/raincloud-communications/' target="_blank" rel="noopener noreferrer"><BsLinkedin className='w-5 h-5 mx-auto'></BsLinkedin></Link></button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='mr-24'>
+                            <h1 className="text-5xl text-white font-bold -mt-72 leading-[3.5rem]">We are an <br /> Advertising & <br /> Marketing firm.</h1>
+                            <p className="py-6 text-white">Sed non mauris vitae erat consequat auctor eu in elit. Class <br /> aptent taciti sociosqu ad litora torquent per conubia nostra.</p>
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     );
