ac36d344414177a73a8d3bc174dfd107b6110711
Raincloud <rhn.raincloud@gmail.com>
2023-09-10 16:32:51 +0600

    some delete

--- src/Components/Pages/Homes/Home.js
+++ src/Components/Pages/Homes/Home.js
@@ -4,14 +4,12 @@ import Message from '../Messages/Message';
 import Team from '../Teams/Team';
 import Client from '../Clients/Client';
 import Header from '../../Shared/NavBar/Header/Header';
-import Goal from '../OurGoal/Goal';
 import Work from '../Works/Work';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
-            {/* <Goal></Goal> */}
             <Service></Service>
             <Client></Client>
             <Work></Work>
